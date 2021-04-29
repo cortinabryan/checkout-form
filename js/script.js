67d3b02fd16e332739720bd39b3f835200fbda4e
unknown <cortinabryan@gmail.com>
2021-04-28 17:54:39 -0700

    updated ccv validation code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const form = document.getElementById("form");
 const ccv = document.getElementById("ccv");
+const checkCircle = document.getElementById("check-circle");
+const exclamationCircle = document.getElementById("exclamation-circle");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -15,21 +17,43 @@ function checkInputs() {
     //show error
     //add error classes
     setErrorFor(ccv);
+  } else if (ccvValue.length === 3) {
+    setSuccessFor(ccv);
   } else {
     // add success class
-    setSuccessFor(ccv);
+    setErrorFor(ccv);
   }
 }
+
+// TRYING TO FIGURE OUT HOW TO CHECK if .length === 3 digits. Kinda stuck rn
+
+// if (ccvValue === "") {
+//   //show error
+//   //add error classes
+//   setErrorFor(ccv);
+// } else if (ccvValue.length === 3 && typeof ccvValue === "Number") {
+//   setSuccessFor(ccv);
+// } else {
+//   // add success class
+//   setErrorFor(ccv);
+// }
+
 function setErrorFor(input) {
-  const formControl = input.parentElement; // MAYBE THE PROBLEM
-  // const small = row.querySelector("small");
-
-  // add error message inside small
-  // small.innerText = message;
-
-  // add error class
-  // row.className = "row-right error";
+  const formControl = input.parentElement;
   formControl.className = "form-control error";
+  exclamationCircle.style.visibility = "visible";
 }
 
-function setSuccessFor(input) {}
+function setSuccessFor(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = "form-control success";
+  checkCircle.style.visibility = "visible";
+}
+
+function setSuccessFor2(input) {
+  const formControl = input.parentElement;
+
+  formControl.className = "form-control success";
+  checkCircle.style.visibility = "visible";
+}
